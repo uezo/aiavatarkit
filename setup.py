1e1c25b12a1c9e4c64a8acc9b198bdcd1f1d3a74
uezo <uezo@uezo.net>
2023-05-29 22:12:30 +0900

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiavatar",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/uezo/aiavatar",
     author="uezo",
     author_email="uezo@uezo.net",
