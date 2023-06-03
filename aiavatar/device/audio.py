f0fbe24c95663d6664dd0278d958c35d393b5a12
uezo <uezo@uezo.net>
2023-06-03 11:12:37 +0900

    Support interactive audio device selection
    Use like below👍
    ```python
    INPUT_DEVICE = -1   # Index of input audio device
    
    if INPUT_DEVICE < 0:
        # Select with prompt
        input_device_info = AudioDevice.get_input_device_with_prompt()
    else:
        # Use configured device
        input_device_info = AudioDevice.get_device_info(INPUT_DEVICE)
    
    input_device = input_device_info["index"]
    ```
    
    Also terminate PyAudio instances after using them internally.

--- aiavatar/device/audio.py
+++ aiavatar/device/audio.py
@@ -3,15 +3,24 @@ from pyaudio import PyAudio
 class AudioDevice:
     @classmethod
     def get_default_input_device_info(cls):
-        return PyAudio().get_default_input_device_info()
+        pa = PyAudio()
+        ret = pa.get_default_input_device_info()
+        pa.terminate()
+        return ret
 
     @classmethod
     def get_default_output_device_info(cls):
-        return PyAudio().get_default_output_device_info()
+        pa = PyAudio()
+        ret = pa.get_default_output_device_info()
+        pa.terminate()
+        return ret
 
     @classmethod
     def get_device_info(cls, index: int):
-        return PyAudio().get_device_info_by_index(index)
+        pa = PyAudio()
+        ret = pa.get_device_info_by_index(index)
+        pa.terminate()
+        return ret
 
     @classmethod
     def get_input_device_by_name(cls, name: str):
@@ -30,11 +39,36 @@ class AudioDevice:
         return None
 
     @classmethod
+    def get_input_device_with_prompt(cls, prompt: str=None):
+        print("==== Input devices ====")
+        for d in AudioDevice.get_audio_devices():
+            if d["maxInputChannels"] > 0:
+                print(f'{d["index"]}: {d["name"]}')
+        idx = input(prompt or "Index of microphone device (Skip to use default): ")
+        if idx == "":
+            return cls.get_default_input_device_info()
+        else:
+            return cls.get_device_info(int(idx))
+
+    @classmethod
+    def get_output_device_with_prompt(cls, prompt: str=None):
+        print("==== Output devices ====")
+        for d in AudioDevice.get_audio_devices():
+            if d["maxOutputChannels"] > 0:
+                print(f'{d["index"]}: {d["name"]}')
+        idx = input(prompt or "Index of speaker device (Skip to use default): ")
+        if idx == "":
+            return cls.get_default_output_device_info()
+        else:
+            return cls.get_device_info(int(idx))
+
+    @classmethod
     def get_audio_devices(cls):
         devices = []
         pa = PyAudio()
         for i in range(pa.get_device_count()):
             devices.append(pa.get_device_info_by_index(i))
+        pa.terminate()
         return devices
 
     @classmethod
