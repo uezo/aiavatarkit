4843fdd2c2390126abd4744d39848d4cecb756c9
uezo <uezo@uezo.net>
2023-06-23 19:00:58 +0900

    Fix bug that the value for channels is invalid

--- aiavatar/listeners/wakeword.py
+++ aiavatar/listeners/wakeword.py
@@ -4,8 +4,8 @@ from typing import Callable
 from . import SpeechListenerBase
 
 class WakewordListener(SpeechListenerBase):
-    def __init__(self, api_key: str, wakewords: list, on_wakeword: Callable, volume_threshold: int=3000, timeout: float=0.3, min_duration: float=0.2, max_duration: float=2, lang: str="ja-JP", rate: int=44100, device_index: int=-1, verbose: bool=False):
-        super().__init__(api_key, self.invoke_on_wakeword, volume_threshold, timeout, 0.0, min_duration, max_duration, lang, rate, device_index)
+    def __init__(self, api_key: str, wakewords: list, on_wakeword: Callable, volume_threshold: int=3000, timeout: float=0.3, min_duration: float=0.2, max_duration: float=2, lang: str="ja-JP", rate: int=44100, chennels: int=1, device_index: int=-1, verbose: bool=False):
+        super().__init__(api_key, self.invoke_on_wakeword, volume_threshold, timeout, 0.0, min_duration, max_duration, lang, rate, chennels, device_index)
         self.wakewords = wakewords
         self.on_wakeword = on_wakeword
         self.verbose = verbose
