bdc4535ae76cf67915ba2ff5e9ff32c27f3b7d68
uezo <uezo@uezo.net>
2023-06-23 19:04:26 +0900

    Add support for Function Calling⚡️
    Use `chat_processor.add_function` to use ChatGPT function calling. In this example, `get_weather` will be called autonomously.
    
    ```python
    # Add function
    async def get_weather(location: str):
        await asyncio.sleep(1.0)
        return {"weather": "sunny partly cloudy", "temperature": 23.4}
    
    app.chat_processor.add_function(
        name="get_weather",
        description="Get the current weather in a given location",
        parameters={
            "type": "object",
            "properties": {
                "location": {
                    "type": "string"
                }
            }
        },
        func=get_weather
    )
    ```
    
    And, after `get_weather` called, message to get voice response will be sent to ChatGPT internally.
    
    ```json
    {
        "role": "function",
        "content": "{\"weather\": \"sunny partly cloudy\", \"temperature\": 23.4}",
        "name": "get_weather"
    }
    ```

--- aiavatar/processors/chatgpt.py
+++ aiavatar/processors/chatgpt.py
@@ -1,11 +1,37 @@
 from logging import getLogger, NullHandler
 import traceback
-from typing import Iterator
+import json
+from typing import Iterator, Callable
 from openai import ChatCompletion
 from . import ChatProcessor
 
+class ChatGPTFunction:
+    def __init__(self, name: str, description: str=None, parameters: dict=None, func: Callable=None):
+        self.name = name
+        self.description = description
+        self.parameters = parameters
+        self.func = func
+    
+    def get_spec(self):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "parameters": self.parameters
+        }
+
+
+class ChatCompletionStreamResponse:
+    def __init__(self, stream: Iterator[str], function_name: str=None):
+        self.stream = stream
+        self.function_name = function_name
+
+    @property
+    def response_type(self):
+        return "function_call" if self.function_name else "content"
+
+
 class ChatGPTProcessor(ChatProcessor):
-    def __init__(self, api_key: str, model: str="gpt-3.5-turbo", temperature: float=1.0, max_tokens: int=0, system_message_content: str=None, history_count: int=10):
+    def __init__(self, api_key: str, model: str="gpt-3.5-turbo-0613", temperature: float=1.0, max_tokens: int=0, functions: dict=None, system_message_content: str=None, history_count: int=10):
         self.logger = getLogger(__name__)
         self.logger.addHandler(NullHandler())
 
@@ -13,12 +39,41 @@ class ChatGPTProcessor(ChatProcessor):
         self.model = model
         self.temperature = temperature
         self.max_tokens = max_tokens
+        self.functions = functions or {}
         self.system_message_content = system_message_content
         self.history_count = history_count
         self.histories = []
 
+    def add_function(self, name: str, description: str=None, parameters: dict=None, func: Callable=None):
+        self.functions[name] = ChatGPTFunction(name=name, description=description, parameters=parameters, func=func)
+
     def reset_histories(self):
         self.histories.clear()
+
+    async def chat_completion_stream(self, messages, call_functions: bool=True):
+        params = {
+            "api_key": self.api_key,
+            "messages": messages,
+            "model": self.model,
+            "temperature": self.temperature,
+            "stream": True,
+        }
+        if self.max_tokens:
+            params["max_tokens"] = self.max_tokens
+
+        if call_functions and self.functions:
+            params["functions"] = [v.get_spec() for _, v in self.functions.items()]
+
+        stream_resp = ChatCompletionStreamResponse(await ChatCompletion.acreate(**params))
+
+        async for chunk in stream_resp.stream:
+            if chunk:
+                delta = chunk["choices"][0]["delta"]
+                if delta.get("function_call"):
+                    stream_resp.function_name = delta["function_call"]["name"]
+                break
+        
+        return stream_resp
 
     async def chat(self, text: str) -> Iterator[str]:
         try:
@@ -28,26 +83,48 @@ class ChatGPTProcessor(ChatProcessor):
             messages.extend(self.histories[-1 * self.history_count:])
             messages.append({"role": "user", "content": text})
 
-            params = {
-                "api_key": self.api_key,
-                "messages": messages,
-                "model": self.model,
-                "temperature": self.temperature,
-                "stream": True
-            }
-            if self.max_tokens:
-                params["max_tokens"] = self.max_tokens
+            response_text = ""
+            stream_resp = await self.chat_completion_stream(messages)
 
-            completion = await ChatCompletion.acreate(**params)
-            
-            response_text = "" 
-            async for chunk in completion:
-                if chunk:
-                    content = chunk["choices"][0]["delta"].get("content")
+            async for chunk in stream_resp.stream:
+                delta = chunk["choices"][0]["delta"]
+                if stream_resp.response_type == "content":
+                    content = delta.get("content")
+                    if content:
+                        response_text += delta["content"]
+                        yield content
+
+                elif stream_resp.response_type == "function_call":
+                    function_call = delta.get("function_call")
+                    if function_call:
+                        arguments = function_call["arguments"]
+                        response_text += arguments
+
+            if stream_resp.response_type == "function_call":
+                self.histories.append(messages[-1])
+                self.histories.append({
+                    "role": "assistant",
+                    "function_call": {
+                        "name": stream_resp.function_name,
+                        "arguments": response_text
+                    },
+                    "content": None
+                })
+
+                api_resp = await self.functions[stream_resp.function_name].func(**json.loads(response_text))
+
+                messages.append({"role": "function", "content": json.dumps(api_resp), "name": stream_resp.function_name})
+
+                response_text = ""
+                stream_resp = await self.chat_completion_stream(messages, False)
+
+                async for chunk in stream_resp.stream:
+                    delta = chunk["choices"][0]["delta"]
+                    content = delta.get("content")
                     if content:
                         response_text += content
                         yield content
-            
+                
             if response_text:
                 self.histories.append(messages[-1])
                 self.histories.append({"role": "assistant", "content": response_text})
