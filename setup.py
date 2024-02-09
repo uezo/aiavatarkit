f10df714e900d9af3cdd54604fb0046798226a84
uezo <uezo@uezo.net>
2024-02-09 21:03:43 +0900

    Update OpenAI client library to 1.12.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiavatar",
-    version="0.2.1",
+    version="0.3.0",
     url="https://github.com/uezo/aiavatar",
     author="uezo",
     author_email="uezo@uezo.net",
@@ -12,7 +12,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["examples*", "tests*"]),
-    install_requires=["aiohttp", "numpy", "openai", "sounddevice"],
+    install_requires=["aiohttp==3.8.4", "numpy==1.24.3", "openai==1.12.0", "sounddevice==0.4.6"],
     license="Apache v2",
     classifiers=[
         "Programming Language :: Python :: 3"
