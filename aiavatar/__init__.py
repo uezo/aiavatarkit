356352b127834d1e65efe329bef6feb2a4b7e7bd
uezo <uezo@uezo.net>
2024-04-20 12:49:28 +0900

    Add setting up logging

--- aiavatar/__init__.py
+++ aiavatar/__init__.py
@@ -1,3 +1,12 @@
+import logging
+
+logger = logging.getLogger("aiavatar")
+logger.setLevel(logging.INFO)
+log_format = logging.Formatter("[%(levelname)s] %(asctime)s : %(message)s")
+streamHandler = logging.StreamHandler()
+streamHandler.setFormatter(log_format)
+logger.addHandler(streamHandler)
+
 # Device
 from .device import AudioDevice
 # Processor
