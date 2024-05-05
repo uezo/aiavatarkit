5ece05d26192f2e24617a1f20222955879bd7c76
uezo <uezo@uezo.net>
2024-05-05 13:45:52 +0900

    Fix AzureWakewordListener network reconnection issue #40
    This commit addresses network reconnection issues identified in both the AzureWakewordListener and the AzureVoiceRequestListener. Previously, these components failed to resume their functions after experiencing network changes. This fix ensures that both components can detect network status changes and reinitialize appropriately, maintaining continuous operation across all platforms.
    
    - Implemented network detection and automatic reinitialization for both AzureWakewordListener and AzureVoiceRequestListener.
    - Unified the reconnection handling logic to ensure consistent behavior on macOS and Windows.
    - Conducted extensive testing to verify that both components now handle network disruptions and reconnect effectively without losing functionality.
    
    The resolution of these issues enhances the reliability and usability of the voice assistant in unstable network environments.

--- aiavatar/listeners/azurewakeword.py
+++ aiavatar/listeners/azurewakeword.py
@@ -3,13 +3,14 @@ import asyncio
 from logging import getLogger, NullHandler
 import queue
 from threading import Thread
+import time
 import traceback
 from typing import Callable
 import azure.cognitiveservices.speech as speechsdk
 from . import WakewordListenerBase
 
 class AzureWakewordListener(WakewordListenerBase):
-    def __init__(self, api_key: str, region: str, wakewords: list, on_wakeword: Callable, lang: str="ja-JP", device_name: str=None, verbose: bool=False):
+    def __init__(self, api_key: str, region: str, wakewords: list, on_wakeword: Callable, lang: str="ja-JP", device_name: str=None, restart_interval: float=5.0, verbose: bool=False):
         self.logger = getLogger(__name__)
         self.logger.addHandler(NullHandler())
 
@@ -28,8 +29,11 @@ class AzureWakewordListener(WakewordListenerBase):
         self.wakewords = wakewords
         self.on_wakeword = on_wakeword
         self.recognized_queue = queue.Queue()
+        self.restart_interval = restart_interval
         self.verbose = verbose
-    
+
+        self.is_processing_wakeword = False
+
     def on_recognized(self, evt):
         recognized_text = evt.result.text.replace("。", "").replace("、", "").replace("!", "").replace("！", "").replace("?", "").replace("？", "").strip()
 
@@ -49,11 +53,28 @@ class AzureWakewordListener(WakewordListenerBase):
     def disable_recognition(self):
         self.speech_recognizer.recognized.disconnect_all()
 
+    def on_canceled(self, evt):
+        self.logger.warning(f"Continuous recognition was canceled for some reasons.(e.g. network disconnection).: {evt}")
+        self.disable_recognition()
+
+        self.logger.info(f"Restart recognition after {self.restart_interval} seconds...")
+        time.sleep(self.restart_interval)
+
+        # Setup recognizer
+        self.speech_recognizer = speechsdk.SpeechRecognizer(speech_config=self.speech_config, audio_config=self.audio_config, language=self.lang)
+        self.speech_recognizer.canceled.connect(lambda evt: self.on_canceled(evt))
+
+        # Start and enable recognition
+        self.speech_recognizer.start_continuous_recognition()
+        if not self.is_processing_wakeword:
+            self.enable_recognition()
+
     async def start_listening(self):
         try:
             # Setup recognizer before start
             if self.speech_recognizer is None:
                 self.speech_recognizer = speechsdk.SpeechRecognizer(speech_config=self.speech_config, audio_config=self.audio_config, language=self.lang)
+                self.speech_recognizer.canceled.connect(lambda evt: self.on_canceled(evt))
 
             self.speech_recognizer.start_continuous_recognition()
             self.enable_recognition()
@@ -66,10 +87,13 @@ class AzureWakewordListener(WakewordListenerBase):
 
                 # Process recognized text
                 self.disable_recognition()
+                self.is_processing_wakeword = True
                 try:
                     await self.on_wakeword(recognized_text)
                 except Exception as ex:
                     self.logger.error(f"Error at on_wake: {ex}\n{traceback.format_exc()}")
+                finally:
+                    self.is_processing_wakeword = False
                 self.recognized_queue.task_done()
                 self.enable_recognition()
         
